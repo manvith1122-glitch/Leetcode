f79e322acab9ef6e7534e567be633692538a8b9e
manvith1122-glitch <manvithyanama@gmail.com>
2026-07-20 19:38:50 +0530

    Time: 7 ms (83.75%), Space: 11.9 MB (23.68%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,14 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,int> mp;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            if(mp.find(temp)!=mp.end()){
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
             }
-            mp[temp]++;
-            temp=temp->next;
         }
         return false;
     }
